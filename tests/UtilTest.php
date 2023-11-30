25f134a17dbc387cc3b1305796fd1b8bd7d5adec
nolan450 <80448885+nolan450@users.noreply.github.com>
2023-11-30 16:29:25 +0100

    ajout de test

---
+++ tests/UtilTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class UtilTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        // on vérifie que 1 + 1 = 2
+        $this->assertEquals(2, 1 + 1);
+    }
+}
